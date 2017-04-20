735199d9b16a29ae85d4a18538b6102aa295b36b
farantir <jakob.hippe@googlemail.com>
2017-04-20 16:45:46 +0200

    menu bar jetzt mit js generierbar

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+function test()
+{
+   test = menubar();
+   test.add(menubar_Item("Test1"));
+   test.add(menubar_Item("Test2"));
+   test.add(menubar_Item("Test3"));
+   test.add(menubar_Item("Test4"));
+   test.make_Visible();
+}
+
+
 function menubar_Item(name,onclick)
 {
     li = document.createElement("li");
@@ -5,17 +16,31 @@ function menubar_Item(name,onclick)
     li.text = document.createTextNode(name);
     li.onclick = onclick;
 
-    li.appendChild(a);
-    a.appendChild(text);
+    li.appendChild(li.a);
+    li.a.appendChild(li.text);
     return li;
 }
 
 function menubar()
 {
     menu = document.createElement("ul");
+    //menu.style.display = "none";
+    menu.classList.add("menubar");
+    document.body.appendChild(menu);
+    
     menu.add = function(entry)
     {
-        this.menu.appendChild(entry);
+        this.appendChild(entry);
     }
+
+    menu.make_Visible = function()
+    {
+        for(m of document.getElementsByClassName("menubar"))
+        {
+            m.style.display = "none";
+        }
+        this.style.display = "";
+    }
+
     return menu;
 }
